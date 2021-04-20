3773c92b4e41128a95f5021a28f8e406815d1c70
naily26 <nailymasruri@gmail.com>
2021-04-20 16:07:37 +0700

    Praktik 1: menambahkan function register dengan parameter RegisterRequest pada Api/AuthController.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,4 +9,20 @@ class AuthController extends Controller
 {
     use ApiResponse;
     //
+
+    public function register(RegisterRequest $request){
+        $validated = $request->validated();
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return $this->apiSuccess([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'User' => $user,
+        ]);
+    }
 }
