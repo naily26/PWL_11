4e63bec7e63ddceef1271f93031d0f28b3ba2b6b
naily26 <nailymasruri@gmail.com>
2021-04-20 15:53:41 +0700

    Praktik 1:  mengubah kode pada ApiRequest

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -3,9 +3,15 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Auth\Events\Validated;
 
-class ApiRequest extends FormRequest
+
+
+abstract class ApiRequest extends FormRequest
 {
+    use ApiResponse;
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -21,10 +27,17 @@ class ApiRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
-    {
-        return [
-            //
-        ];
+    abstract public function rules();
+    protected function failedValidation(Validator $validator){
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+    protected function failedAuthorization(){
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
     }
 }
